fc4f171e8d6f38c079de851ad7010a57aa2af565
garick-dev <73178499+garick-dev@users.noreply.github.com>
2021-07-21 23:34:06 +0300

    Checked processor speed

--- findYourProcessorSpeed/test.js
+++ findYourProcessorSpeed/test.js
@@ -1,14 +1,13 @@
 const os = require("os");
 
-
 const foo = () => {
-
-    const myPC = os.cpus(); 
-    const model = myPC[0].model;
-     console.log(model);
-    const speed = myPC[0].speed;
-     console.log(speed);
-
-}
+  const myPC = os.cpus();
+  const model = myPC[0].model;
+  console.log(model);
+  const speed = myPC[0].speed;
+  console.log(speed);
+};
 
 module.exports.impOS = foo;
+
+// Check processor speed
