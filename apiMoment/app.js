993d573096164489a429070c63a5c078f6cb80aa
garick-dev <73178499+garick-dev@users.noreply.github.com>
2021-07-23 23:56:34 +0300

    Get api from moment

---
+++ apiMoment/app.js
@@ -0,0 +1,10 @@
+const moment = require("moment");
+
+let a = moment();
+let b = moment([1997, 01, 07]);
+
+let years = a.diff(b, "years");
+// let months = moment().add(years).moment().a.diff(b, "month");
+// let days = years + months + (a.diff(b, "days"));
+
+console.log(months);
